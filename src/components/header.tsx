6d96638d36d3454ecaa8a371ff66f3096dc9a1b7
Jacob Kepford <rtbredge@gmail.com>
2023-04-01 16:21:30 -0500

    reworked nav

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,37 +3,34 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <nav>
-      <div className="flex flex-wrap justify-between">
-        <Link href="/" className="ml-4 mt-4 text-3xl">
+    <nav className="ml-4 flex flex-wrap items-center">
+      <div>
+        <Link href="/" className="text-3xl">
           Wordle Tracker
         </Link>
-        <ul className="mr-4 mt-4 flex flex-row space-x-2 text-xl">
-          <li>
-            <Link href="/" className="rounded py-4 px-8 hover:bg-slate-800">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/upload"
-              className="inline rounded py-4 px-8 hover:bg-slate-800"
-            >
-              Upload
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/userscore"
-              className="rounded py-4 px-8 hover:bg-slate-800"
-            >
-              Your Scores
-            </Link>
-          </li>
-          <li>
-            <UserButton />
-          </li>
-        </ul>
+      </div>
+      <div className="ml-10 mt-1.5 flex flex-row items-center space-x-4">
+        <Link
+          href="/"
+          className="text-1xl rounded px-4 py-4 hover:bg-slate-800"
+        >
+          Home
+        </Link>
+        <Link
+          href="/upload"
+          className="text-1xl rounded px-4 py-4 hover:bg-slate-800"
+        >
+          Upload
+        </Link>
+        <Link
+          href="/userscore"
+          className="text-1xl rounded px-4 py-4 hover:bg-slate-800"
+        >
+          Your Scores
+        </Link>
+      </div>
+      <div className="ml-auto mr-4">
+        <UserButton />
       </div>
     </nav>
   );
