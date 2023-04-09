80153fa7545dda91c2cbcfef791aed242792352b
Jacob Kepford <rtbredge@gmail.com>
2023-04-09 16:27:40 -0500

    started creating new proc for scores

--- src/server/api/routers/score.ts
+++ src/server/api/routers/score.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
-export const exampleRouter = createTRPCRouter({
-  hello: publicProcedure
+export const scoreRouter = createTRPCRouter({
+  addScore: publicProcedure
     .input(z.object({ text: z.string() }))
     .query(({ input }) => {
       return {
