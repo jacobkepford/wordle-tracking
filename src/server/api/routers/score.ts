a8ac51f2d904331e87a669fc9d29e711ab6290de
Jacob Kepford <rtbredge@gmail.com>
2023-05-28 15:47:11 -0500

    Adding score functionality added, prisma schema modified

--- src/server/api/routers/score.ts
+++ src/server/api/routers/score.ts
@@ -1,14 +1,25 @@
 import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
 
 export const scoreRouter = createTRPCRouter({
-  addScore: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
+  addScore: privateProcedure
+    .input(z.object({ score_value: z.number(), score_date: z.date() }))
+    .mutation(async ({ input, ctx }) => {
+      await ctx.prisma.score.upsert(
+        { where: {score_user: ctx.userId, score_date: input.score_date},
+          create:
+          {
+            score_value: input.score_value,
+            score_user: ctx.userId,
+            score_date: input.score_date
+          },
+          update: 
+          {
+            score_value: input.score_value
+          }
+        }
+      ); 
     }),
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.score.findMany();
